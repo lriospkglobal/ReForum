28933097fd9095119a7ce862405a4fa35f98cdd9
Armando Rios Romano <68391554+lriospkglobal@users.noreply.github.com>
2020-10-29 19:31:49 -0700

    Update api.js

--- backend/entities/discussion/api.js
+++ backend/entities/discussion/api.js
@@ -43,11 +43,11 @@ const discussionAPI = (app, client) => {
   //create new mosaic
   app.post('/api/discussion/newMosaic', (req, res) => {
     return new Promise((resolve, reject) => {
-      //return axios.post('http://localhost:5500/api-mosaic/build-mosaic?forumName=' + req.body.forumName + '&tileId=' + req.body.tileId + '&forumId=' + req.body.forumId + '&tileSize=3&enlargement=1&quality=100&email=' + encodeURIComponent(req.body.user.email))
-      return axios.post('http://mosaic-python-api.herokuapp.com/api-mosaic/build-mosaic?forumName=' + 
-      req.body.forumName + '&tileId=' + req.body.tileId + '&forumId=' + req.body.forumId + 
-      '&tileSize=3&enlargement=1&quality=100&email=' + encodeURIComponent(req.body.user.email) +
-      '&discussionTitle='+encodeURIComponent(req.body.discussionTitle))
+      //return axios.post('http://localhost:5500/api-mosaic/build-mosaic?forumName=' + 
+      return axios.post('http://mosaic-python-api.herokuapp.com/api-mosaic/build-mosaic?forumName=' +
+        req.body.forumName + '&tileId=' + req.body.tileId + '&forumId=' + req.body.forumId +
+        '&tileSize=3&enlargement=1&quality=100&email=' + encodeURIComponent(req.body.user.email) +
+        '&discussionTitle=' + encodeURIComponent(req.body.discussionTitle))
         .then(req => {
           resolve(res.send(req.data.message))
         }).catch(err => console.error(err))
@@ -70,7 +70,7 @@ const discussionAPI = (app, client) => {
 
       });
       res.on('finish', () => {
-        //return axios.post('http://localhost:3000/api/discussion/newMosaic', { forumId: req.body.forumId, user: req.user, tileId, forumName: req.body.forumName })
+        //return axios.post('http://localhost:3000/api/discussion/newMosaic', { forumId: req.body.forumId, user: req.user, tileId, forumName: req.body.forumName, discussionTitle: req.body.title })
         return axios.post('https://mosaic-forum.herokuapp.com/api/discussion/newMosaic', { forumId: req.body.forumId, user: req.user, tileId, forumName: req.body.forumName, discussionTitle: req.body.title })
           .then(() => console.log('Success!')).catch(err => console.error(err))
       });
